b842cb38dd3b2c5f56fcc046e60b802878b771e1
Pavlo Stepchenko <pstep4e23@gmail.com>
2025-05-06 11:42:00 +0300

    add compose

--- main.py
+++ main.py
@@ -1,16 +1,14 @@
-# This is a sample Python script.
+import os
+import requests
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+OLLAMA_HOST = os.getenv('OLLAMA_HOST', 'http://localhost:11434')
 
+response = requests.post(
+    f"{OLLAMA_HOST}/api/generate",
+    json={
+        "model": "mistral",
+        "prompt": "your prompt"
+    }
+)
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+print(response.json())
